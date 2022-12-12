2f1d681b71d60b46b8ad14c70c1ee9c8644777aa
Dhreetiman <dhreetiman09999@gmail.com>
2022-12-12 16:44:02 +0530

    readme added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const route = require("./route/route")
+const app = express()
+
+app.use(express.json())
+
+app.use("/", route)
+
+
+
+mongoose.connect("mongodb+srv://Lucifer:lucifer123@mycluster.bdqxxtr.mongodb.net/group11Database?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+}).then(()=>console.log("MongoDB Connected"))
+.catch((err)=>console.log(err))
+
+app.listen(3000, ()=>{
+    console.log("Server runnig on port",3000)
+})
