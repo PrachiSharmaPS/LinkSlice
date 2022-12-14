83b7d6fac7f12387e447a6cc2eb627b500596faf
ashishingle29 <ashishingle589@gmail.com>
2022-12-14 19:42:19 +0530

    Redis Cache

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,8 +1,10 @@
 const urlModel = require("../model/urlModel")
 const shortid = require('shortid');
 const axios = require("axios")
- 
+const redis = require('redis')
+const { promisify } = require('util')
 
+ 
 //=================validation===========================//
 
 const valid = function (value) {
@@ -14,7 +16,28 @@ const valid = function (value) {
     }
     return true;
   };
-  
+ 
+//========================[Apply redis and connect]================
+
+const redisConnect = redis.createClient(
+   14652,
+   "redis-14652.c299.asia-northeast1-1.gce.cloud.redislabs.com",
+   {no_ready_check: true}
+);
+
+redisConnect.auth("uMq15mB5TBzteVVvb4sAZEIhrtHbe3Sp", function(err){
+    if(err) throw err;
+});
+
+redisConnect.on("connect", async function(){
+    console.log("Redis is Connected...");
+});
+
+
+const GET_ASYNC = promisify(redisConnect.GET).bind(redisConnect);
+const SET_ASYNC = promisify(redisConnect.SETEX).bind(redisConnect);
+
+
 
 //========================[Function for Create Shorten URL]==========================//
 
@@ -61,11 +84,22 @@ const urlShort = async (req, res) => {
 
 let getData = async (req, res) => {
     try{
+        let cahcedProfileData = await GET_ASYNC(`${req.params.urlCode}`)
+    
+if(cahcedProfileData) {
+   let txt =  JSON.parse(cahcedProfileData)
+    res.status(302).redirect(txt.longUrl)
+  } else{
+
         let urlCode = req.params.urlCode
     if(!shortid.isValid(urlCode)) return res.status(400).send({status: false, msg: "Please provide a valid URL code"}) 
+
     let findLongUrl = await urlModel.findOne({urlCode: urlCode});
     if (!findLongUrl) return res.status(404).send({status: false, msg: "Url code not found"})
+
+    await SET_ASYNC(`${req.params.urlCode}`,24*60*60, JSON.stringify(findLongUrl))
     res.status(302).redirect(findLongUrl.longUrl)
+  }
     }catch(err){
         return res.status(500).send({status:false,message:err.message})
     }
