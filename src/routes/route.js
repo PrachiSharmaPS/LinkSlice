952dd8650ca6f7662f4784944b0393e412cba469
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2025-10-07 08:36:48 +0530

    add some test api end pint

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const router = express.Router()
-//const urlController = require("../controller/urlController")
+const urlController = require("../controller/urlController")
 const getEmployees  =require("../controller/test")
 const getTime  =require("../controller/timetake")
 
 router.post("/api/employees", getEmployees.getEmployees);
 router.get("/api/employees", getEmployees.getEmployees);
 router.get("/api/getTime", getTime.getTime);
-//router.post("/url/shorten",urlController.urlShort )
+router.post("/url/shorten",urlController.urlShort )
 
-//router.get("/:urlCode", urlController.getData)
+router.get("/:urlCode", urlController.getData)
 
 router.all('/*', function(req,res){
     return res.status(400).send({message: "Invalid HTTPS Request"})
